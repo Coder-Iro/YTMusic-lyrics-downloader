37ab207d4da859891b6eb24935fa443c62f3524a
Socio-path <doohee006@gmail.com>
2020-02-04 17:37:01 +0900

    Modulize lyrics.py

--- lyrics.py
+++ lyrics.py
@@ -1,17 +1,73 @@
-from mutagen.id3 import ID3, USLT, SLT
-import sys
+#!/usr/bin/env python3.8
+from mutagen.id3 import ID3, SLT, APIC, TIT2, TPE1, TIT3
+import os
 import webvtt
+import youtube_dl
+import json
+from subprocess import Popen
 
-lyrics = webvtt.read(sys.argv[2])
-lyri = []
-lyr = []
-for lyric in lyrics:
-    times = [int(x) for x in lyric.start.replace(".", ":").split(":")]
-    ms = times[-1]+1000*times[-2]+1000*60*times[-3]+1000*60*60*times[-4]
-    lyri.append((lyric.text,ms))
-    lyr.append(lyric.text)
-fil = ID3(sys.argv[1])
-tag = SLT(encoding=3, lang='kor', format=2, type=1, text=lyri)
-print(tag)
-fil.add(tag)
-fil.save(v1=0)
+def hoook(d):
+    global musicname
+    global lyricsname
+    global thumbsname
+    global metaname
+    if d['status'] == 'finished':
+        musicname = d['filename'].replace(".webm",".mp3")
+        lyricsname = d['filename'].replace(".webm",".ko.vtt")
+        thumbsname = d['filename'].replace(".webm",".jpg")
+        metaname = d['filename'].replace(".webm",".info.json")
+
+def conv_to_ms(times):
+    time = [int(x) for x in times.replace(".", ":").split(":")]
+    ms = time[-1]+1000*time[-2]+1000*60*time[-3]+1000*60*60*time[-4]
+    return ms
+
+ydl_opts = {
+        'format' : 'bestaudio/best',
+        'extractaudio' : True,
+        'audioformat' : 'mp3',
+        'postprocessors' : [{
+            'key' : 'FFmpegExtractAudio',
+            'preferredcodec' : 'mp3',
+            'preferredquality' : '0'
+            },],
+        'writeinfojson' : True,
+        'writethumbnail' : True,
+        'writesubtitles' : True,
+        'subtitlesformat' : 'vtt',
+        'subtitleslangs' : ['ko'],
+        'progress_hooks' : [hoook],
+        'prefer_ffmpeg' : True,
+        }
+ydl = youtube_dl.YoutubeDL(ydl_opts)
+
+def download(url):
+    musicname = ''
+    lyricsname = ''
+    thumbsname = ''
+    metaname = ''
+    ydl.download([url])
+    lyrics = webvtt.read(lyricsname)
+    lyri = [('',0)]
+    for lyric in lyrics:
+        lyri.append((lyric.text,conv_to_ms(lyric.start)))
+        lyri.append(("",conv_to_ms(lyric.end)))
+    fil = ID3(musicname)
+    tag = SLT(encoding=3, lang='kor', format=2, type=1, text=lyri)
+    fil.add(tag)
+    fil.add(APIC(encoding=3, mime='image/jpeg', data=open(thumbsname,"rb").read()))
+    with open(metaname, 'r') as f:
+        meta = json.load(f)
+    fil.add(TIT2(encoding=3, text=meta["title"]))
+    fil.add(TIT3(encoding=3, text=meta["description"]))
+    fil.add(TPE1(encoding=3, text=meta["uploader"]))
+    fil.save(v1=0)
+    Popen(f'ffmpeg -i "{lyricsname}" "{lyricsname.replace(".vtt",".lrc")}"',shell=True).wait()
+    os.remove(thumbsname)
+    os.remove(lyricsname)
+    os.remove(metaname)
+    return musicname
+
+if __name__ == "__main__":
+    import sys
+    download(sys.argv[1])
